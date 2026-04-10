d7366eeae51a9d45207b4cc0eab0fba4962d24bf
samarth-vboxuser <chindesamarth@gmail.com>
2026-04-10 05:49:25 +0000

    Version 1 of aws_resource_report.sh script.

---
+++ aws_resource_report.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+################################################
+# Author: Samarth Chinde
+# version: v1
+#
+# This script will report the aws resource useage
+################################################
+
+date=$(date)
+echo "This report is of date $date."
+
+echo " "
+
+#list s3 buckets
+echo "list of s3 buckets:-"
+aws s3 ls
+aws s3 ls > aws_resouce_report_document
+
+echo " "
+
+#list ec2 instances
+echo "list of ec2 instances:-"
+#aws ec2 describe-instances
+aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
+aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId' >> aws_resouce_report_document
+
+echo " "
+
+#list lambda functions
+echo "list of lambda functions:-"
+aws lambda list-functions
+aws lambda list-functions >> aws_resouce_report_document
+
+echo " "
+
+#list IAM users
+echo "list of iam users:-"
+#aws iam list-users
+aws iam list-users | jq '.Users[].UserId'
+aws iam list-users | jq '.Users[].UserId' >> aws_resouce_report_document
+
+echo " "
+
+echo "File is created check aws_resouce_report_document."
